e7f23aa7dfc3c1a0fa9f5d1f9d1963cc7d44e53d
NicholasAshenfelter <nashenfelter@gmail.com>
2025-09-24 03:51:09 -0400

    Initial Commit

---
+++ src/main/java/com/nicholasashenfelter/ActalentProject/ActalentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nicholasashenfelter.ActalentProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActalentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActalentProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicholasashenfelter/ActalentProject/ActalentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicholasashenfelter.ActalentProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActalentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
